53d5da13c5a5d57d6cc54204fb309c69e1cf7204
Zunickx <Zunickx@gmail.com>
2024-03-10 22:52:39 +0300

    Create files html/chat.html, html/modal-help.html, html/modal-login.html, html/modal-logout.html

---
+++ src/index.js
@@ -0,0 +1,6 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable eol-last */
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
+import './js/main';
+import './css/styles.css';
